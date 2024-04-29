057b63c3baac455afa01a9af3e0cb54638d52153
JhonZapTa <167173015+JhonZapTa@users.noreply.github.com>
2024-04-29 11:57:07 +0200

    Cambio github

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int num = sc.nextInt();
         System.out.println("Hola mundo de nuevo");
         for (int i = 0; i < 3; i++) {
             System.out.println("Hola ¿Cómo estas?");
