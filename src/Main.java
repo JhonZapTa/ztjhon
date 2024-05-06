7d41ae7bcad4bda8a8e8816a1cef439e3dc1e9fe
JhonZapTa <jhon.zapata1@educa.madrid.org>
2024-05-06 11:09:46 +0200

    Primera version

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un número: ");
         int num = sc.nextInt();
+        System.out.println("El triple del número " + num + " es: " + tripleDelNum(num));
         System.out.println("Hola mundo de nuevo");
         for (int i = 0; i < 3; i++) {
             System.out.println("Hola ¿Cómo estas?");
         }
+    }
+    public static int tripleDelNum (int num) {
+        return num * 3;
     }
 }
